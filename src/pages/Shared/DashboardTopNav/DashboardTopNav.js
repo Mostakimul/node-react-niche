b96f7282a6d61e52cb2bac5e819c385d9492af72
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-11 19:55:11 +0100

    showing username top dashboard nav

--- src/pages/Shared/DashboardTopNav/DashboardTopNav.js
+++ src/pages/Shared/DashboardTopNav/DashboardTopNav.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const DashboardTopNav = () => {
+  const { user } = useAuth();
   return (
     <div className="hidden md:block">
-      <div className="container bg-red-50 py-4 flex justify-end  ">
+      <div className="container bg-red-50 py-4 flex justify-end  items-center">
+        <span className="text-green-700 font-semibold font-display mx-2">
+          {user?.displayName}
+        </span>
         <Link
           to="/"
           className="bg-red-100 hover:bg-red-200 py-1 px-2 rounded shadow"
