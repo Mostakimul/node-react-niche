178293e598759c2720d417d063179cc3b7a12bb9
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-12 14:48:09 +0100

    showing all product in manage product

--- src/pages/Dashboard/Admin/ManageProduct/ManageProduct.js
+++ src/pages/Dashboard/Admin/ManageProduct/ManageProduct.js
@@ -1,16 +1,67 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import DashboardTopNav from '../../../Shared/DashboardTopNav/DashboardTopNav';
+import LoaderSpin from '../../../Shared/LoaderSpin/LoaderSpin';
+import SingleProduct from './SingleProduct';
 
 const ManageProduct = () => {
+  const [products, setProducts] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get('http://localhost:5000/products')
+      .then((res) => {
+        // console.log(res.data);
+        setProducts(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="flex-1">
       <DashboardTopNav />
-      <div className="flex-1">
-        <div className="bg-gray-100 m-4 p-2 rounded shadow">
-          <h3 className="text-center font-bold text-gray-800 text-2xl">
-            Manage Product
-          </h3>
-        </div>
+      {/* Page Title */}
+      <div className="bg-gray-100 m-4 p-2 rounded shadow">
+        <h3 className="text-center font-bold text-gray-800 text-2xl">
+          Manage Product
+        </h3>
+      </div>
+      {/* Page Body */}
+      <div className="bg-gray-100 m-4 p-2 rounded shadow">
+        {isLoading ? (
+          <LoaderSpin />
+        ) : (
+          <table className="w-4/5 mx-auto text-center border-collapse border border-green-800">
+            <thead>
+              <tr>
+                <th className="border border-green-600 p-2">#No</th>
+                <th className="border border-green-600 p-2">Product Name</th>
+                <th className="border border-green-600 p-2">Quantity</th>
+                <th className="border border-green-600 p-2">Image</th>
+                <th className="border border-green-600 p-2">Price</th>
+                <th className="border border-green-600 p-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <SingleProduct
+                  key={product._id}
+                  product={product}
+                  index={index}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
---
+++ src/pages/Dashboard/Admin/ManageProduct/SingleProduct.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SingleProduct = ({ product, index }) => {
+  return (
+    <tr>
+      <td className="border border-green-600 p-2">{index + 1}</td>
+      <td className="border border-green-600 p-2">{product.name}</td>
+      <td className="border border-green-600 p-2">{product.quantity}</td>
+      <td className="border border-green-600 p-2">
+        <img src={product.image} alt="product" className="w-10 mx-auto" />
+      </td>
+      <td className="border border-green-600 p-2">$ {product.price}</td>
+      <td className="border border-green-600 p-2">
+        <button className="py-1 px-2 mx-1 bg-blue-700 hover:bg-blue-600 text-sm text-gray-50 rounded shadow">
+          Edit
+        </button>
+        <button className="py-1 px-2 mx-1 bg-red-700 hover:bg-red-600 text-sm text-gray-50 rounded shadow">
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default SingleProduct;
