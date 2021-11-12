7da21b55423d49c1f3e805ba0b9e54199f4c9921
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-12 22:26:15 +0100

    only 6 products in home page

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainNavbar from '../../Shared/Navbar/MainNavbar';
 import Banner from '../Banner/Banner';
+import Products from '../Products/Products';
 
 const Home = () => {
   return (
     <div>
       <MainNavbar />
       <Banner />
+      <Products />
     </div>
   );
 };
---
+++ src/pages/Home/Products/Products.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import LoaderSpin from '../../Shared/LoaderSpin/LoaderSpin';
+import SingleProduct from '../../Shop/SingleProduct';
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get('http://localhost:5000/productsHome')
+      .then((res) => {
+        setProducts(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <div className="container">
+        <h2 className="font-display font-semibold text-center text-4xl pt-5">
+          Our Products
+        </h2>
+        {isLoading && <LoaderSpin />}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 py-10">
+          {products.map((product) => (
+            <SingleProduct key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
