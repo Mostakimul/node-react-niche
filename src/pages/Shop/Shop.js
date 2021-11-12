4d950a12c1ad832fe5a9b89e6e6fe2fd45f1445b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-12 21:40:57 +0100

    shop page implemented

---
+++ src/pages/Shop/Shop.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import LoaderSpin from '../Shared/LoaderSpin/LoaderSpin';
+import MainNavBar from '../Shared/Navbar/MainNavbar';
+import PageTitle from '../Shared/PageTitle/PageTitle';
+import SingleProduct from './SingleProduct';
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get('http://localhost:5000/products')
+      .then((res) => {
+        setProducts(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <MainNavBar />
+      <PageTitle title="SHOP" />
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 py-10">
+          {isLoading && <LoaderSpin />}
+          {products.map((product) => (
+            <SingleProduct key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
---
+++ src/pages/Shop/SingleProduct.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SingleProduct = ({ product }) => {
+  const { name, image, price, quantity } = product;
+  return (
+    <div className="bg-red-50 m-5 md:m-10 rounded-sm shadow transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ">
+      <div>
+        <img src={image} alt={name} className="w-3/4 mx-auto" />
+      </div>
+      <div className="text-center bg-gray-50 m-2 p-2 rounded shadow">
+        <h2 className="font-display font-semibold text-xl">{name}</h2>
+        <div className="flex justify-around items-center my-2">
+          <p className="font-display text-base">Price: ${price}</p>
+          <p className="font-display text-base">Qty: {quantity}</p>
+        </div>
+      </div>
+      <div className="m-2">
+        <button className="font-display text-lg bg-green-500 hover:bggren block w-full rounded shadow">
+          Buy Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProduct;
