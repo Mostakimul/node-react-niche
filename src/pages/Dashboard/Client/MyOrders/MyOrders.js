b055b84a56c199aeb03475529dc6565d2327f49e
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-13 03:13:43 +0100

    my orders showed in dash board

--- src/pages/Dashboard/Client/MyOrders/MyOrders.js
+++ src/pages/Dashboard/Client/MyOrders/MyOrders.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../../hooks/useAuth';
 import DashboardTopNav from '../../../Shared/DashboardTopNav/DashboardTopNav';
+import LoaderSpin from '../../../Shared/LoaderSpin/LoaderSpin';
+import ModalConfirm from '../../../Shared/ModalConfirm/ModalConfirm';
+import SuccessAlert from '../../../Shared/SuccessAlert/SuccessAlert';
+import MySingleOrder from './MySingleOrder';
 
 const MyOrders = () => {
+  const { user } = useAuth();
+  const [orders, setOrders] = useState([]);
+
+  // states dependency
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [productId, setProductId] = useState('');
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [deleteStatus, setDeleteStatus] = useState(false);
+  const [status, setStatus] = useState(false);
+
+  // Close Success
+  const closeSuccess = () => {
+    setIsSuccess(false);
+  };
+  // show modal
+  const handleModal = (productId) => {
+    setProductId(productId);
+    setShowModal(true);
+  };
+  // close modal
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  // fecth orders
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`http://localhost:5000/orders/${user.email}`)
+      .then((res) => {
+        setOrders(res.data);
+        setIsLoading(false);
+        setStatus(!status);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [deleteStatus]);
+
+  const deleteProduct = (pdId) => {
+    setIsLoading(true);
+    axios
+      .delete(`http://localhost:5000/products/${pdId}`)
+      .then((res) => {
+        if (res.data.deletedCount === 1) {
+          setIsSuccess(true);
+          setDeleteStatus(!deleteStatus);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    setShowModal(false);
+  };
+
   return (
     <div className="flex-1">
       <DashboardTopNav />
@@ -11,6 +80,52 @@ const MyOrders = () => {
           <h3 className="text-center font-bold text-gray-800 text-2xl">
             My Orders
           </h3>
+        </div>
+        {/* Page Body */}
+        <div className="bg-gray-100 m-4 p-2 rounded shadow">
+          {isSuccess && (
+            <div className="w-full md:w-4/5 mx-auto my-4">
+              <SuccessAlert
+                message="Product Deleted Successfully!!!"
+                closeSuccess={closeSuccess}
+              ></SuccessAlert>
+            </div>
+          )}
+          {isLoading ? (
+            <LoaderSpin />
+          ) : (
+            <table className="w-full md:w-4/5 mx-auto text-center border-collapse border border-green-800">
+              <thead>
+                <tr>
+                  <th className="border border-green-600 p-2">#No</th>
+                  <th className="border border-green-600 p-2">Product Name</th>
+                  <th className="border border-green-600 p-2">Quantity</th>
+                  <th className="border border-green-600 p-2">Image</th>
+                  <th className="border border-green-600 p-2">Price</th>
+                  <th className="border border-green-600 p-2">Status</th>
+                  <th className="border border-green-600 p-2">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((order, index) => (
+                  <MySingleOrder
+                    key={order._id}
+                    index={index}
+                    order={order}
+                    handleModal={handleModal}
+                  />
+                ))}
+              </tbody>
+            </table>
+          )}
+          {showModal && (
+            <ModalConfirm
+              showModal={showModal}
+              closeModal={closeModal}
+              deleteProduct={deleteProduct}
+              productId={productId}
+            />
+          )}
         </div>
       </div>
     </div>
---
+++ src/pages/Dashboard/Client/MyOrders/MySingleOrder.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const MySingleOrder = ({ index, order, handleModal }) => {
+  const { _id, productId, status, qty } = order;
+  const [product, setProduct] = useState({});
+  // fetch product
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/products/${productId}`)
+      .then((res) => {
+        if (res.data) {
+          let orderData = res.data;
+          orderData.status = status;
+          orderData.qty = qty;
+          setProduct(orderData);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <tr>
+      <td className="border border-green-600 p-2">{index + 1}</td>
+      <td className="border border-green-600 p-2">{product.name}</td>
+      <td className="border border-green-600 p-2">{product.qty}</td>
+      <td className="border border-green-600 p-2">
+        <img src={product.image} alt="product" className="w-10 mx-auto" />
+      </td>
+      <td className="border border-green-600 p-2">$ {product.price}</td>
+      <td className="border border-green-600 ">
+        <span
+          className={`${
+            product.status ? 'bg-red-400' : 'bg-blue-400'
+          } rounded py-1 px-2 p-2`}
+        >
+          {product.status}
+        </span>
+      </td>
+      <td className="border border-green-600 p-2">
+        <button
+          onClick={() => handleModal(_id)}
+          className="py-1 px-2 my-1 md:my-0 mx-1 bg-red-700 hover:bg-red-600 text-sm text-gray-50 rounded shadow"
+        >
+          Delete order
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default MySingleOrder;
