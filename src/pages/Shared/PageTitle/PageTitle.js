faff865d4a94ab4cbbabd8fd4e7293d08add0d14
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-13 06:09:06 +0100

    page title font changed

--- src/pages/Shared/PageTitle/PageTitle.js
+++ src/pages/Shared/PageTitle/PageTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PageTitle = ({ title }) => {
   return (
     <div className="bg-page-header bg-cover bg-center bg-no-repeat">
-      <h2 className="py-14 text-3xl text-white text-center font-display font-bold">
+      <h2 className="py-14 text-6xl text-white text-center font-display font-bold">
         {title}
       </h2>
     </div>
